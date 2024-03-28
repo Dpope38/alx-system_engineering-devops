21d0b4e742bf50b8512aacbf9b152fdf20bbb403
popeopzy <popeopzy@gmail.com>
2024-03-28 02:22:33 +0100

    install fask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,8 @@ package { 'python3-pip':
 }
 
 exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => '/usr/local/bin:/usr/bin',
-  refreshonly => true,
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/usr/bin'],
+  environment => [],
+  require     => Package['python3-pip'],
 }
