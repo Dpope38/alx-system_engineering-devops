a16b0b22f5a28c4d46c79bcd112511e072f7f13d
popeopzy <popeopzy@gmail.com>
2024-03-28 02:15:03 +0100

    error correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
 #Install flask "a python web application library" and pip3
 
-class { 'python::pip':
-  ensure  => 'present',
+package { 'python3-pip':
+  ensure => installed,
 }
 
-python::pip { 'flask':
-  ensure  => '2.1.0',
-  require => Class['python::pip'],
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => '/usr/local/bin:/usr/bin',
+  refreshonly => true,
 }
